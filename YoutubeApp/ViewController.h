629729b5feb3723bec92fcac312f5f28843d1559
Andi Dehelean <andi.deh30@icloud.com>
2016-11-19 12:11:19 +0200

    Created startup project

---
+++ YoutubeApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YoutubeApp
+//
+//  Created by Dehelean Andrei on 11/19/16.
+//  Copyright © 2016 Dehelean Andrei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YoutubeApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YoutubeApp
+//
+//  Created by Dehelean Andrei on 11/19/16.
+//  Copyright © 2016 Dehelean Andrei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
